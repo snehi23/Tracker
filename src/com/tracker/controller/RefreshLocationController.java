d2e7153e2bac37cb004f2ecc54116e0f985e4ba2
Snehal <snehalshendware1@gmail.com>
2014-05-09 17:40:25 +0530

    Eighth Commit [9 may 2014] from cisco office

--- src/com/tracker/controller/RefreshLocationController.java
+++ src/com/tracker/controller/RefreshLocationController.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,13 +29,14 @@ public class RefreshLocationController extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
 	
-	List<StationLocation> stationlocation = new ArrayList<StationLocation>();
+	Connection conn=null;
+	PreparedStatement prep= null;
+	
+	List<StationLocation> stationLocation = new ArrayList<StationLocation>();
     
     
     ServletContext ctx=getServletContext();
-    PreparedStatement ps = null;
-    ResultSet rs= null;
-    
+   
     String connectionURL= ctx.getInitParameter("dbURL");
     String uname= ctx.getInitParameter("dbUser");
     String pwd= ctx.getInitParameter("dbPassword");
@@ -50,54 +52,98 @@ public class RefreshLocationController extends HttpServlet {
 		
 	}
 	
+   
 	
 	try {
     	
-    	Connection conn = (Connection) ctx.getAttribute("DBConnection");
-    	ps = conn.prepareStatement("select station_name from station_lat_long where station_lat_long_id<10");
-    	rs = ps.executeQuery();
-    	while(rs.next()) {
-    		StationLocation loc = new StationLocation();
-    		loc.setStation_name(rs.getString("station_name"));
-    		stationlocation.add(loc);		
-    	}
+		conn = (Connection) ctx.getAttribute("DBConnection");
     	
-    	for(StationLocation st : stationlocation) {
+    	conn.setAutoCommit(false);
+    	
+    	Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+    	
+    	ResultSet rs1 = stmt.executeQuery("select t.From_Station from tracker t where not exists (select * from station_lat_long s where s.Station_Code=t.From_Station)");
+    	
+    	
+    	if(rs1.equals(null)) {
     		
-    		System.out.println(st.getStation_name());
+    		rs1.close();
+        	conn.commit();
+    		
+    	} else {
+    		
+    		JAXBContext jc = JAXBContext.newInstance(FetchLatLong.class);
+            
+            
+            String mapurl1 = "https://maps.googleapis.com/maps/api/geocode/xml?address=";
+            String mapurl2 = "&sensor=false&key=AIzaSyAGd0G-I1ZepBC2X5oR1DmAJRMS9aPF4G0";
+    		
+    		ResultSet rs2 = stmt.executeQuery("select distinct Station_Name,Station_Code from station_code  where Station_Code in (select t.From_Station from tracker t where not exists (select * from station_lat_long s where s.Station_Code=t.From_Station))");
+
+    		while(rs2.next()) {
+
+    			StationLocation t = new StationLocation();
+    			t.setStation_name(rs2.getString(1));
+    			t.setStation_code(rs2.getString(2));
+    			
+    			Unmarshaller unmarshaller = jc.createUnmarshaller();
+    	         String temp =mapurl1+t.getStation_name()+mapurl2;
+    	         temp = temp.replaceAll(" ","%20").trim();
+    	         URL url = new URL(temp);
+    	         System.out.println(temp);
+    	         
+    	         InputStream xmlStream = url.openStream();
+    	         FetchLatLong latlong = (FetchLatLong) unmarshaller.unmarshal(xmlStream);
+    	         
+    			t.setLatitude(latlong.getResult().getGeo().getLocation().getLat());
+    			t.setLongitude(latlong.getResult().getGeo().getLocation().getLng());
+    			
+    			stationLocation.add(t);	
+    			
+    			
+        	}
+    		
+    		for(StationLocation s1 : stationLocation) {
+    			
+    			System.out.println(s1.getStation_code()+" "+s1.getStation_name()+" "+s1.getLatitude()+" "+s1.getLongitude());
+    		}
+    		
+    		rs2.close();
+        	conn.commit();
+    		
+    	}
+
+		}catch(Exception E2) {
+    	
+			E2.printStackTrace();
+        		
+		}
+	
+    	try {
+
+    	
+    	
+    	for(StationLocation s1 : stationLocation) {
+    	String sql= "insert into station_lat_long(station_lat_long_id,station_code,station_name,latitude,longitude) values (?,?,?,?,?)";
+    	prep = conn.prepareStatement(sql);
+    	prep.setInt(1, 0);
+    	prep.setString(2, s1.getStation_code()); 	
+    	prep.setString(3, s1.getStation_name());
+    	prep.setDouble(4, s1.getLatitude());
+    	prep.setDouble(5, s1.getLongitude());
+    	prep.executeUpdate();
     	}
     	
-    	request.setAttribute("station_location", stationlocation);
-    	
-    	JAXBContext jc = JAXBContext.newInstance(FetchLatLong.class);
-        
-        
-        String mapurl1 = "https://maps.googleapis.com/maps/api/geocode/xml?address=";
-        String mapurl2 = "&sensor=false&key=AIzaSyAGd0G-I1ZepBC2X5oR1DmAJRMS9aPF4G0";
-        
-        for(StationLocation sl : stationlocation) {
-         Unmarshaller unmarshaller = jc.createUnmarshaller();
-         String temp =mapurl1+sl.getStation_name()+mapurl2;
-         temp = temp.replaceAll(" ","%20").trim();
-         URL url = new URL(temp);
-         System.out.println(temp);
-         //URL url = new URL("https://maps.googleapis.com/maps/api/geocode/xml?address=Nagpur&sensor=false&key=AIzaSyAGd0G-I1ZepBC2X5oR1DmAJRMS9aPF4G0");
-         InputStream xmlStream = url.openStream();
-         FetchLatLong latlong = (FetchLatLong) unmarshaller.unmarshal(xmlStream);
-         	System.out.println(sl.getStation_name());
-             System.out.println("Latitude : "+latlong.getResult().getGeo().getLocation().getLat());
-             System.out.println("Longitude : "+latlong.getResult().getGeo().getLocation().getLng());
-             
-        }
-    	
-    	rs.close();
-    	ps.close();
+    	conn.commit();
+    	prep.close();
     	conn.close();
-    		 	
-    }catch(Exception E2) {
-        		
-    }
-	
+    	
+    	}catch(Exception E3) {
+        	
+        	E3.printStackTrace();
+            		
+        }
+
 	RequestDispatcher rd = null;
 	 
     rd = request.getRequestDispatcher("/displayinfo.jsp");
